914b201193fe8e05c08873217d541c23718b8b23
Hans Yulian <hansyulian@windowslive.com>
2017-12-06 23:33:08 +0800

    initial commit

---
+++ utility.js
@@ -0,0 +1,38 @@
+function createQueryStrings(query) {
+    var result = "";
+    var first = true;
+    for (var name in query) {
+        if (first)
+            first = !first;
+        else
+            result += "&";
+        result += name + "=" + query[name];
+    }
+    return result;
+}
+
+function getQueryStrings() {
+
+    var url = document.location;
+    var queryString = url.search.substring(1);
+    if (queryString == "")
+        return {};
+    var queries = queryString.split("&");
+    var result = {};
+    for (var i = 0; i < queries.length; i++) {
+        var splitting = queries[i].split("=");
+        var name = splitting[0];
+        var value = splitting[1];
+        result[name] = value;
+    }
+    return result;
+}
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    };
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+};
